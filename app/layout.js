c664d66826e075c491ad6cfe71f508058a23304a
giri-prashant <giriprashant66@gmail.com>
2026-05-17 20:48:28 +0545

    Google analytics script added

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RYVZ90Z0JH"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RYVZ90Z0JH');
+          `
+        }} />
+
         {/*
           Inline styles for the loader so they apply BEFORE globals.css downloads.
           This ensures the loader is visible on very first paint.
